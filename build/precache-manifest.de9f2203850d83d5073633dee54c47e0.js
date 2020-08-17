8f86dddd1b07f7d142526684b0a8613f37cff271
nitinsharmacs <ramujha266@gmail.com>
2020-08-17 11:22:57 +0530

    deploying for netlify

---
+++ build/precache-manifest.de9f2203850d83d5073633dee54c47e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f205f20b44f1940d6600f3efe26801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c22656d7a0abb59c28c6",
+    "url": "/static/css/main.54d100e3.chunk.css"
+  },
+  {
+    "revision": "b872065e3a0480f4da6c",
+    "url": "/static/js/2.317f7f09.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.317f7f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22656d7a0abb59c28c6",
+    "url": "/static/js/main.135cfe02.chunk.js"
+  },
+  {
+    "revision": "3142e8cd5c3c41e82bc7",
+    "url": "/static/js/runtime-main.df40c3be.js"
+  }
+]);
